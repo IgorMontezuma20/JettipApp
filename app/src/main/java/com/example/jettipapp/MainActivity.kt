47ea727d8d190f42533817b89d7434da79b76f85
imiranda <igor.miranda@foursys.com.br>
2022-11-24 15:57:09 -0300

    adicionando corner superior

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -3,35 +3,59 @@ package com.example.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jettipapp.ui.theme.JettipAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JettipAppTheme {
-                MyApp {
-                    Text(text = "Hello Again!")
-
-                }
+            MyApp {
+                Text(text = "Hello Again!")
             }
         }
     }
 }
 
 @Composable
-fun MyApp(Content: @Composable () -> Unit) {
-    Surface(
-        color = MaterialTheme.colors.background
-    ) {
+fun MyApp(content: @Composable () -> Unit) {
+    JettipAppTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
     }
+}
+
+
+@Preview
+@Composable
+fun TopHeader(){
+    androidx.compose.material.Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
+        //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+    ) {
+        Column() {
+            
+        }
+        
+    }
+    
 }
 
 
@@ -39,6 +63,8 @@ fun MyApp(Content: @Composable () -> Unit) {
 @Composable
 fun DefaultPreview() {
     JettipAppTheme {
-
+        MyApp {
+            Text(text = "Hello Again")
+        }
     }
 }
