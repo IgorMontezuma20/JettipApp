6b6aa24215e67b8ca6a4274381075b1248d85e26
imiranda <igor.miranda@foursys.com.br>
2022-11-25 11:45:52 -0300

    top header concluido

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,8 +11,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettipapp.ui.theme.JettipAppTheme
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello Again!")
+                TopHeader()
             }
         }
     }
@@ -43,19 +43,31 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Preview
 @Composable
-fun TopHeader(){
-    androidx.compose.material.Surface(modifier = Modifier
-        .fillMaxWidth()
-        .height(150.dp)
-        .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
+fun TopHeader(totalPorPessoa: Double = 134.0) {
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFF00BCD4)
         //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
     ) {
-        Column() {
-            
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPorPessoa)
+            Text(text = "Total Por Pessoa", style = MaterialTheme.typography.h5)
+            Text(
+                text = "R$ $total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold)
+
         }
-        
+
     }
-    
+
 }
 
 
